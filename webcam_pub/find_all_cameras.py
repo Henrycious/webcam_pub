e28ab0377a7202deacde7a44ec358085047dd74f
Henrycious <henry.gennet@gmail.com>
2022-02-08 11:24:29 +0100

    Fixed error
    new folder structure in Ubuntu got considered

--- webcam_pub/find_all_cameras.py
+++ webcam_pub/find_all_cameras.py
@@ -2,22 +2,24 @@
 
 import re
 import os
+from os import path
 
 def get_camera(camera_name):
     cam_num = None
     for file in os.listdir("/sys/class/video4linux"):
-        real_file = os.path.realpath("/sys/class/video4linux/" + file + "/name")
+        if not path.exists("/sys/class/video4linux/" + file + "/device/input/"):
+            continue
+        input_name = os.listdir("/sys/class/video4linux/" + file + "/device/input/")
+        real_file = os.path.join("/sys/class/video4linux/" + file + "/device/input/"+ input_name[0] + "/name")
         with open(real_file, "rt") as name_file:
             name = name_file.read().rstrip()
+            print(name)
         if camera_name in name:
-            cam_num = int(re.search("\d+$", file).group(0))
-            found = "FOUND!"
-            return cam_num
+            port_return = file[-1]
+            return port_return
             break
         else:
-            found = "      "
-        print("{} {} -> {}".format(found, file, name))
-    return cam_num
+            found = ""
+
 
 cam_number = get_camera('xxx')
-print(cam_number)
